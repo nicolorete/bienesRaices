9ddb4185e11274b760569243d5e8ea764e18a86d
Nicolas Coronel <ncoronel@aplay.com.ar>
2024-06-17 17:04:13 -0300

    pagina de Categorias y barra de busqueda

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,29 +1,111 @@
 import {Categoria, Precio, Propiedad} from '../models/index.js'
+import { Sequelize } from 'sequelize'
 
 const inicio = async (req, res) =>{
 
-    const [ categorias, precios ] = await Promise.all([
+    const [ categorias, precios, casas, departamentos ] = await Promise.all([
         Categoria.findAll({raw: true}),
         Precio.findAll({raw: true}),
+        Propiedad.findAll({
+            limit: 3,
+            where: {
+                categoriaId: 1
+            },
+            include: [
+                {
+                    model: Precio,
+                    as: 'precio'
+                }
+            ],
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        }),
+        Propiedad.findAll({
+            limit: 3,
+            where: {
+                categoriaId: 2
+            },
+            include: [
+                {
+                    model: Precio,
+                    as: 'precio'
+                }
+            ],
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        })
     ])
 
     res.render('inicio', {
         pagina: 'Inicio',
         categorias,
-        precios
+        precios,
+        casas,
+        departamentos,
+        csrfToken: req.csrfToken()
     })
 }
 
-const categoria = (req, res) =>{
+const categoria = async (req, res) =>{
+    const { id } = req.params
 
+    // Comprobar que la categoria exista
+    const categoria = await Categoria.findByPk(id)
+    if(!categoria){
+        return res.redirect('/404')
+    }
+
+    // Obtener las propiedades de la categoria
+    const propiedades = await Propiedad.findAll({
+        where: {
+            categoriaId : id
+        },
+        include: [
+            { model: Precio, as:'precio' }
+        ]
+    })
+
+    res.render('categoria', {
+        pagina: `${categoria.nombre} en Venta `, 
+        propiedades,
+        csrfToken: req.csrfToken()
+    })
 }
 
 const noEncontrado = (req, res) =>{
-
+    res.render('404', {
+        pagina: 'No Encontrada',
+        csrfToken: req.csrfToken()
+    })
 }
 
-const buscador = (req, res) =>{
+const buscador = async (req, res) =>{
+    const { termino } = req.body
 
+    // Validar que el termino no este vacio
+    if(!termino.trim()){
+        res.redirect('back')
+    }
+
+    // Consultar las propiedades 
+    const propiedades = await Propiedad.findAll({
+        where: {
+            titulo: {
+                [Sequelize.Op.like] : "%" + termino + "%"
+            }
+        },
+        include: [
+            { model: Precio, as: "precio"}
+        ]
+    })
+
+    res.render('busqueda', {
+        pagina: `Resultados de la búsqueda: ${termino} `, 
+        propiedades,
+        csrfToken: req.csrfToken()
+    })
 }
 
 export {
